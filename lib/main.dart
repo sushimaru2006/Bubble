3c306ce7115e8cc08bd23f58a92384a011ab42c1
Ikumaru Hamanaka <m21122@g.metro-cit.ac.jp>
2024-02-22 15:37:07 +0900

    pause

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:arkit_plugin/arkit_plugin.dart';
-import 'package:flutter/rendering.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:vector_math/vector_math_64.dart';
-import 'package:webview_flutter/webview_flutter.dart';
 import 'dart:math';
-
+import 'dart:typed_data';
 
 void main() => runApp(MaterialApp(home: MyApp()));
 
@@ -15,24 +13,17 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  // arkitのコントローラー
   late ARKitController arkitController;
+  late InAppWebViewController inAppWebViewController;
 
-  final controller = Completer<WebViewController>();
 
-  // 画像のパス
+  final uri =  WebUri("https://read.amazon.co.jp/manga/B07QQPYZ2R?sample=true&ref_=kwl_kr_iv_rec_1");
   final List<String> paths = ["assets/book/page1.jpeg", "assets/book/page2.jpeg", "assets/book/page3.jpeg", "assets/book/page4.jpeg"];
-  // 画像パスのインデックス
+
   int pathIndex = 0;
-
-  // arNode
   late ARKitNode node;
-
-  // Nodeの位置と回転
   Vector3 pos = Vector3(0, -0.1, -0.1);
   Vector3 rot = Vector3(0, -20, 0);
-
-  // カメラの位置からの相対的な位置と回転
   Vector3 relativePosition = Vector3(-0.2, -0.1, -0.1);
   Vector3 relativeRotation = Vector3(0, -20, 0);
 
@@ -42,30 +33,52 @@ class _MyAppState extends State<MyApp> {
     super.dispose();
   }
 
-  @override
-  Widget build(BuildContext context) => Scaffold(
-    appBar: AppBar(title: const Text('ARKit in Flutter')),
-    body: ARKitSceneView(onARKitViewCreated: onARKitViewCreated),
-    floatingActionButton: FloatingActionButton(
-      onPressed: () {
-        // カメラの座標を取得
+  Widget _buildFloatingActionButton() {
+    return GestureDetector(
+      onTap: () {
         arkitController.cameraPosition().then((camPos) {
-          // カメラの回転を取得
           arkitController.getCameraEulerAngles().then((camRot) {
-            // nodeの場所を計算。自分の周囲0.2の円周上を動かす
             pos = Vector3(relativePosition.x * sin(camRot.y), relativePosition.y, relativePosition.z * cos(camRot.y)) + camPos!;
             node.position = pos;
-
-            // nodeの向きを変更
             rot = relativeRotation + Vector3(camRot.y, 0, 0);
             node.eulerAngles = rot;
-
             arkitController.remove("page");
             arkitController.add(node);
           });
         });
       },
+      child: Container(
+        height: 54,
+        width: 54,
+        margin: const EdgeInsets.only(right: 17, bottom: 50),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(50),
+        ),
+        child: const Icon(Icons.add, size: 25),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    body: Stack(
+      children: [
+        InAppWebView(
+          initialUrlRequest: URLRequest(
+            url: uri
+          ),
+          onWebViewCreated:(InAppWebViewController controller) {
+            inAppWebViewController = controller;
+          }
+         // onProgressChanged: ,
+        ),
+        Padding(
+          padding: EdgeInsets.only(right: 20, left: 20),
+          child: ARKitSceneView(onARKitViewCreated: onARKitViewCreated),
+        ),
+      ],
     ),
+    floatingActionButton: _buildFloatingActionButton(),
     bottomNavigationBar: BottomNavigationBar(
       items: const <BottomNavigationBarItem>[
         BottomNavigationBarItem(
@@ -81,16 +94,15 @@ class _MyAppState extends State<MyApp> {
     ),
   );
 
-  void onARKitViewCreated(ARKitController arkitController) async {
+  void onARKitViewCreated(ARKitController arkitController) {
     this.arkitController = arkitController;
     node = ARKitNode(
       geometry: ARKitPlane(
         height: 0.2,
         width: 0.2,
-        materials : [
+        materials: [
           ARKitMaterial(
-            // diffuse: ARKitMaterialProperty.image(paths[pathIndex]),
-            diffuse: ARKitMaterialProperty.image("https://picsum.photos/250?image=9"),
+            diffuse: ARKitMaterialProperty.image(paths[pathIndex]),
             doubleSided: true,
           ),
         ],
@@ -108,13 +120,44 @@ class _MyAppState extends State<MyApp> {
       if (pathIndex < 0) {
         pathIndex = paths.length - 1;
       }
-    }
-    else {
+    } else {
       pathIndex += 1;
       if (pathIndex == paths.length) {
         pathIndex = 0;
       }
     }
+  void _takeWebViewScreenshot() async {
+  // InAppWebViewControllerからスクリーンショットを取得
+    Uint8List? screenshotBytes = await inAppWebViewController.takeScreenshot();
+    if (screenshotBytes != null) {
+    // スクリーンショットを使用して画像を作成
+    // Image image = Image.memory(screenshotBytes);
+
+    // 画像を表示
+    this.arkitController = arkitController;
+    node = ARKitNode(
+      geometry: ARKitPlane(
+        height: 0.2,
+        width: 0.2,
+        materials: [
+          ARKitMaterial(
+            diffuse: ARKitMaterialImage(
+              Uint8List.fromList(screenshotBytes).toString()
+            ),
+            doubleSided: true,
+          ),
+        ],
+      ),
+      name: "page",
+      position: pos,
+      eulerAngles: rot,
+    );
+    arkitController.add(node);
+  } else {
+    // エラー処理
+    print('Failed to capture screenshot');
+  }
+}
     arkitController.remove("page");
     onARKitViewCreated(arkitController);
   }
