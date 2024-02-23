4bca54824e26c5c62ad66908f334cddf93fb30c3
Ikumaru Hamanaka <m21122@g.metro-cit.ac.jp>
2024-02-24 08:34:03 +0900

    サンプルの追加

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,7 @@ class Init extends StatelessWidget {
       routes: {
         '/kindleRoute': (context) => Kindle(key),
         '/driveRoute': (context) => GoogleDrive(key),
+        '/sample': (context) => Sample(),
       },
     );
   }
@@ -51,6 +52,11 @@ class HomePage extends StatelessWidget {
               },
               child: const Text('googledrive'),
             ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/sample');
+              },
+              child: const Text('sample'))
           ],
         ),
       ),
@@ -326,3 +332,145 @@ class _ARState extends State<AR> {
     }
   }
 }
+
+class Sample extends StatefulWidget {
+  @override
+  _SampleState createState() => _SampleState();
+}
+
+class _SampleState extends State<Sample> {
+  // arkitのコントローラー
+  late ARKitController arkitController;
+
+  // 画像のパス
+  final List<String> paths = ["assets/book/page1.jpeg", "assets/book/page2.jpeg", "assets/book/page3.jpeg", "assets/book/page4.jpeg"];
+  // 画像パスのインデックス
+  int pathIndex = 0;
+
+  // arNode
+  late ARKitNode node;
+
+  // Nodeの位置と回転
+  Vector3 pos = Vector3(0, -0.12, -0.12);
+  Vector3 rot = Vector3(0, -20, 0);
+
+  // カメラの位置からの相対的な位置と回転
+  Vector3 relativePosition = Vector3(-0.12, -0.12, -0.12);
+  Vector3 relativeRotation = Vector3(0, -20, 0);
+
+  // スライダーの値
+  double sliderValue = -20;
+
+  @override
+  void dispose() {
+    arkitController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    body: Stack(
+      children: [
+        Container(
+          // ARのScene
+          child: ARKitSceneView(onARKitViewCreated: onARKitViewCreated),
+        ),
+        // 上にスライダーを重ねる
+        Positioned(
+          top: 50,
+          left: 50,
+          child: RotatedBox(
+            quarterTurns: 3,
+            child: Slider(
+              value: sliderValue,
+              min: -21,
+              max: -19,
+              onChanged: (value) {
+                setState(() {
+                  sliderValue = value;
+                });
+                relativeRotation.y = value;
+                rot = Vector3(rot.x, value, rot.z);
+                node.eulerAngles = rot;
+                arkitController.remove("page");
+                arkitController.add(node);
+              },
+            ),
+          )
+        )
+      ],
+    ),
+    floatingActionButton: FloatingActionButton(
+      onPressed: () {
+        // カメラの座標を取得
+        arkitController.cameraPosition().then((camPos) {
+          // カメラの回転を取得
+          arkitController.getCameraEulerAngles().then((camRot) {
+            // nodeの場所を計算。自分の周囲0.2の円周上を動かす
+            pos = Vector3(relativePosition.x * sin(camRot.y), relativePosition.y, relativePosition.z * cos(camRot.y)) + camPos!;
+            node.position = pos;
+
+            // nodeの向きを変更
+            rot = relativeRotation + Vector3(camRot.y, 0, 0);
+            node.eulerAngles = rot;
+
+            arkitController.remove("page");
+            arkitController.add(node);
+          });
+        });
+      },
+    ),
+    bottomNavigationBar: BottomNavigationBar(
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.arrow_back),
+          label: 'Back',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.arrow_forward),
+          label: 'Next',
+        ),
+      ],
+      onTap: _onItemTapped,
+    ),
+  );
+
+  void onARKitViewCreated(ARKitController arkitController) async {
+    this.arkitController = arkitController;
+    node = ARKitNode(
+      geometry: ARKitPlane(
+        height: 0.2,
+        width: 0.2,
+        materials : [
+          ARKitMaterial(
+            diffuse: ARKitMaterialProperty.image(paths[pathIndex]),
+            // diffuse: ARKitMaterialProperty.image("https://picsum.photos/250?image=9"),
+            doubleSided: true,
+          ),
+        ],
+      ),
+      name: "page",
+      position: pos,
+      eulerAngles: rot,
+    );
+    arkitController.add(node);
+  }
+
+  void _onItemTapped(int index) {
+    if (index == 0) {
+      pathIndex -= 1;
+      if (pathIndex < 0) {
+        pathIndex = paths.length - 1;
+      }
+    }
+    else {
+      pathIndex += 1;
+      if (pathIndex == paths.length) {
+        pathIndex = 0;
+      }
+    }
+    arkitController.remove("page");
+    onARKitViewCreated(arkitController);
+  }
+}
+
